1d340c86bb9f7d8edd58a5eaebadba2b90f6c5e2
queuecat <2356583659@qq.com>
2022-03-27 16:26:47 +0800

    fix: 使用默认账号登陆mongo

--- server/model/connet.js
+++ server/model/connet.js
@@ -4,7 +4,7 @@
 const mongoose = require('mongoose')
 // 链接数据库
 mongoose
-  .connect('mongodb://user:123456@localhost:27017/todo', {
+  .connect('mongodb://localhost:27017/todo', {
     useNewUrlParser: true
   })
   .then(() => {
